a9a8a389a21564888ff5381e483da54775ae3e09
KireBook <erik19borgnia@gmail.com>
2025-05-20 18:38:41 -0300

    Added persistence folder

---
+++ client/src/main/java/org/grupouno/persistence/JSONSessionPersistence.java
@@ -0,0 +1,4 @@
+package org.grupouno.persistence;
+
+public class JSONSessionPersistence implements ISessionPersistence {
+}
---
+++ client/src/main/java/org/grupouno/persistence/XMLSessionPersistence.java
@@ -0,0 +1,4 @@
+package org.grupouno.persistence;
+
+public class XMLSessionPersistence implements ISessionPersistence {
+}
